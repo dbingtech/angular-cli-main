820386f36d9736e985cccb961374ef8197d0f3c4
dbingtech <95089824+dbingtech@users.noreply.github.com>
2022-08-25 13:22:12 -0400

    82522 Update

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer
+    }, 2000);
+  }
+
+  ngOnInit() {
+    
+  }
 }
